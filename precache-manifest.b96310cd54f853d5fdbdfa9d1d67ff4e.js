6e952197b0fcb2b7a19706464fc09e1218392065
anthonyradose <69907232+anthonyradose@users.noreply.github.com>
2022-08-15 15:34:06 +0000

    Deploying to gh-pages from @ anthonyradose/weather-app@71714bc9f5d29e7d282669d1d78a0768a47b26a1 🚀

--- precache-manifest.b96310cd54f853d5fdbdfa9d1d67ff4e.js
+++ precache-manifest.b96310cd54f853d5fdbdfa9d1d67ff4e.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "2cd5a588a0790a476d48",
+    "revision": "bbf1636dc53330b02011",
     "url": "/weather-app/static/css/main.3a944d60.chunk.css"
   },
   {
-    "revision": "2cd5a588a0790a476d48",
-    "url": "/weather-app/static/js/main.0804e2e2.chunk.js"
+    "revision": "bbf1636dc53330b02011",
+    "url": "/weather-app/static/js/main.19d10617.chunk.js"
   },
   {
     "revision": "214cae62f41d01ec6181",
     "url": "/weather-app/static/js/runtime~main.a6738133.js"
   },
   {
-    "revision": "93295ab9cfe9b8cb152f",
-    "url": "/weather-app/static/js/2.f5ff009e.chunk.js"
+    "revision": "4570a4bc640af73d26fb",
+    "url": "/weather-app/static/js/2.fe5242b6.chunk.js"
   },
   {
     "revision": "c6b1a2cb47ebd29a3dd7",
     "url": "/weather-app/static/js/3.6a1dbab2.chunk.js"
   },
   {
-    "revision": "8869b04f5c2f748f2b2115f7f314e3f7",
+    "revision": "94e69623b2dd51725d0797463707271f",
     "url": "/weather-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.9cf256ce15d1d4446b9432f3cae4c3a0.js"
+  "/weather-app/precache-manifest.b96310cd54f853d5fdbdfa9d1d67ff4e.js"
 );
 
 workbox.clientsClaim();
