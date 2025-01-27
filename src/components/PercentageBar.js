d3f8b27a5e3dfff82782f4f1ec80620d872fc259
Anthony Radose <asradose@gmail.com>
2025-01-27 21:14:21 +0000

    Refactor: Extract PercentageBar styles to CSS file

--- src/components/PercentageBar.js
+++ src/components/PercentageBar.js
@@ -1,25 +1,16 @@
 import React from "react";
-const PercentageBar = (props) => {
-  const { bgcolor, completed } = props;
+import "../App.css";
 
-  const containerStyles = {
-    height: 20,
-    width: "80%",
-    backgroundColor: "#e0e0de",
-    borderRadius: 50,
-  };
-
-  const fillerStyles = {
-    height: "100%",
-    width: `${completed}%`,
-    backgroundColor: bgcolor,
-    borderRadius: "inherit",
-    textAlign: "right",
-  };
-
+const PercentageBar = ({ bgcolor, completed }) => {
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}></div>
+    <div className="percentage-container">
+      <div
+        className="filler"
+        style={{
+          width: `${completed}%`,
+          backgroundColor: bgcolor,
+        }}
+      ></div>
     </div>
   );
 };
